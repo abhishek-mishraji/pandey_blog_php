2918e6e4562f456574e91b6dbe85815d5720335b
Abhishek Mishra <95057637+abhishek-mishraji@users.noreply.github.com>
2024-11-14 00:20:39 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-define("ROOT_URL", "http://localhost/blog/");
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define("ROOT_URL", "https://vivek-blog-4b851a72a738.herokuapp.com/");
+define('DB_HOST', 'ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'u36znupx188lhbpn');
+define('DB_PASS', 'nkpn566oor479rsf');
+define('DB_NAME', 'wno4hk1v0otlgosf');
